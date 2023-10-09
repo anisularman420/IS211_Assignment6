f5c6e98af49a7b2b34eb1f770d017c8376f42ed0
anisularman420 <Anisul.Arman19@spsmail.cuny.edu>
2023-10-08 21:29:26 -0400

    This script defines a new approach to handling unit conversions in a more generalized way. It introduces a convert function that can convert values from one unit to another for various types of units, not just temperature

--- conversions_refactored.py
+++ conversions_refactored.py
@@ -1,1 +1,22 @@
+class ConversionNotPossible(Exception):
+    """Custom exception to handle cases where conversion is not possible."""
+    pass
 
+def convert(fromUnit, toUnit, value):
+    """Convert a value from one unit to another."""
+
+    conversion_factors = {
+        ('Celsius', 'Kelvin'): lambda x: x + 273.15,
+        ('Celsius', 'Fahrenheit'): lambda x: (x * 9/5) + 32,
+        ('Fahrenheit', 'Celsius'): lambda x: (x - 32) * 5/9,
+        ('Fahrenheit', 'Kelvin'): lambda x: (x + 459.67) * 5/9,
+        ('Kelvin', 'Celsius'): lambda x: x - 273.15,
+        ('Kelvin', 'Fahrenheit'): lambda x: (x * 9/5) - 459.67
+    }
+
+    if (fromUnit, toUnit) in conversion_factors:
+        conversion_function = conversion_factors[(fromUnit, toUnit)]
+        result = conversion_function(value)
+        return result
+    else:
+        raise ConversionNotPossible(f"Conversion from {fromUnit} to {toUnit} is not possible.")
