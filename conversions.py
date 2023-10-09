30b43b054b82be47ebdc8eecf72659f00793e55f
anisularman420 <Anisul.Arman19@spsmail.cuny.edu>
2023-10-08 21:26:39 -0400

    This script defines set of functions for tem unit conversions,It includes functions for converting Celsius to Kelvin, Celsius to Fahrenheit, Fahrenheit to Celsius, Fahrenheit to Kelvin, Kelvin to Celsius, and Kelvin to Fahrenheit

--- conversions.py
+++ conversions.py
@@ -1,13 +1,29 @@
-
 def convertCelsiusToKelvin(celsius):
     """Takes in a float representing a Celsius measurement, and returns that temperature converted into Kelvins"""
-    kelvins = 0
-    
+    kelvins = celsius + 273.15
     return kelvins
-
 
 def convertCelsiusToFahrenheit(celsius):
     """Takes in a float representing a Celsius measurement, and returns that temperature converted into Fahrenheit"""
-    fahrenheit = 0
-    
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+def convertFahrenheitToCelsius(fahrenheit):
+    """Takes in a float representing a Fahrenheit measurement, and returns that temperature converted into Celsius"""
+    celsius = (fahrenheit - 32) * 5/9
+    return celsius
+
+def convertFahrenheitToKelvin(fahrenheit):
+    """Takes in a float representing a Fahrenheit measurement, and returns that temperature converted into Kelvins"""
+    kelvins = (fahrenheit + 459.67) * 5/9
+    return kelvins
+
+def convertKelvinToCelsius(kelvin):
+    """Takes in a float representing a Kelvin measurement, and returns that temperature converted into Celsius"""
+    celsius = kelvin - 273.15
+    return celsius
+
+def convertKelvinToFahrenheit(kelvin):
+    """Takes in a float representing a Kelvin measurement, and returns that temperature converted into Fahrenheit"""
+    fahrenheit = (kelvin * 9/5) - 459.67
     return fahrenheit
